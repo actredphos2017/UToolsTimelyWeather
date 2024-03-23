b7e82ea8ae9d2cdccd313232b41f16276a4d3f1c
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-24 00:39:25 +0800

    导入 Element Plus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    .mount('#app')
