bde2e6b04e60d70c25bb30c5d7b6d10491ab0fd0
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-23 22:45:22 +0800

    添加控件，编写基础布局、样式与框架

---
+++ src/components/WeatherPanel.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+import {Comprehensive} from "../models/caiyunapi/comprehensive.ts";
+import {computed, watch} from "vue";
+
+const props = defineProps<{
+  weatherInfo: Comprehensive
+}>();
+
+const weatherInfo = computed(() => props.weatherInfo);
+
+watch(weatherInfo, newValue => {
+  console.log('Update:', newValue)
+}, {immediate: true});
+
+</script>
+
+<template>
+  <div class="content-container">
+    <el-card
+        class="info-card double-size-info"
+        :body-style="{padding: '0px'}"
+        shadow="hover"
+    >
+
+
+
+    </el-card>
+  </div>
+</template>
+
+<style scoped>
+
+.content-container {
+  width: 100%;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  align-items: center;
+  justify-content: center;
+}
+
+.info-card {
+  width: 100%;
+  padding: 8px;
+}
+
+.single-size-info {
+  height: 400px;
+}
+
+.double-size-info {
+  height: 200px;
+}
+
+</style>
