75dcf0d6c9c84e253022c9eb82ecb9adfae8d2ce
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-23 22:43:45 +0800

    添加基础天气数据获取样式

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,74 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  Comprehensive,
+  // getComprehensiveTestFailed,
+  getComprehensiveTestSuccess
+} from "./models/caiyunapi/comprehensive.ts";
+import {onMounted, ref, watch} from "vue";
+import WeatherPanel from "./components/WeatherPanel.vue";
+import {ElLoading} from "element-plus";
+
+const weatherInfo = ref<null | Comprehensive>(null)
+
+onMounted(() => {
+  updateWeather()
+});
+
+const gettingInfo = ref(false);
+
+let loading: any;
+
+watch(gettingInfo, newValue => {
+  if (newValue) {
+    loading = ElLoading.service({
+      lock: true,
+      text: '正在获取天气信息，请稍后……',
+      background: 'rgba(0, 0, 0, 0.7)',
+    });
+  } else {
+    if (loading) {
+      loading.close()
+      loading = null;
+    }
+  }
+}, {immediate: true})
+
+function updateWeather() {
+  gettingInfo.value = true;
+  getComprehensiveTestSuccess('TOKEN')
+      .then(res => {
+        console.log(res);
+        weatherInfo.value = res;
+      })
+      .catch(e => {
+        console.log(e);
+        weatherInfo.value = null;
+      })
+      .finally(() => {
+        gettingInfo.value = false;
+      })
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <WeatherPanel v-if="weatherInfo" :weather-info="weatherInfo"/>
+  <div class="failed-content-container" v-else-if="!gettingInfo" @click="updateWeather">
+    <el-text>获取失败</el-text>
+    <el-button @click="updateWeather">点击重试</el-button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.failed-content-container {
+  display: flex;
+  gap: 8px;
+  flex-direction: column;
+  width: 100%;
+  height: 90vh;
+  align-items: center;
+  justify-content: center
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
