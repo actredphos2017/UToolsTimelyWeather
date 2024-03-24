e9f417ef2062090ee7c219f124f1ef86740e1943
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-24 20:47:56 +0800

    导包 Element Icon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App)
-    .use(ElementPlus)
-    .mount('#app')
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+}
+
+app.use(ElementPlus);
+app.mount('#app');
