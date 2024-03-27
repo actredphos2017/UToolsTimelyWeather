9db4b7073013eb621c2b9a9306b57f6453ad2f12
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-28 00:47:56 +0800

    取消函数日志输出

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -55,7 +55,6 @@ const dayMapper = [
 
 
 export function getFriendlyDateString(dateValue: string | Date | number) {
-    console.log(dateValue)
     const nowDate = new Date();
     const date = new Date(dateValue);
 
@@ -509,4 +508,70 @@ export function getDailyInstance(dailyData: DailyData): DailyInstance[] {
     });
 
     return Object.entries(tempRes).map(e => e[1]);
+}
+
+export function parseWindLevelDescription(windSpeed: number): { levelName: string, desc: string } {
+    let levelName: string;
+    let desc: string;
+
+    if (windSpeed < 1) {
+        levelName = "0 级";
+        desc = "无风";
+    } else if (windSpeed >= 1 && windSpeed < 6) {
+        levelName = "1 级";
+        desc = "微风";
+    } else if (windSpeed >= 6 && windSpeed < 12) {
+        levelName = "2 级";
+        desc = "清风";
+    } else if (windSpeed >= 12 && windSpeed < 20) {
+        levelName = "3 级";
+        desc = "叶摇";
+    } else if (windSpeed >= 20 && windSpeed < 29) {
+        levelName = "4 级";
+        desc = "枝摇";
+    } else if (windSpeed >= 29 && windSpeed < 39) {
+        levelName = "5 级";
+        desc = "强劲";
+    } else if (windSpeed >= 39 && windSpeed < 50) {
+        levelName = "6 级";
+        desc = "强劲";
+    } else if (windSpeed >= 50 && windSpeed < 62) {
+        levelName = "7 级";
+        desc = "超强";
+    } else if (windSpeed >= 62 && windSpeed < 75) {
+        levelName = "8 级";
+        desc = "狂风";
+    } else if (windSpeed >= 75 && windSpeed < 89) {
+        levelName = "9 级";
+        desc = "风啸";
+    } else if (windSpeed >= 89 && windSpeed < 103) {
+        levelName = "10 级";
+        desc = "暴风";
+    } else if (windSpeed >= 103 && windSpeed < 118) {
+        levelName = "11 级";
+        desc = "暴风";
+    } else if (windSpeed >= 118 && windSpeed < 134) {
+        levelName = "12 级";
+        desc = "飓风";
+    } else if (windSpeed >= 134 && windSpeed < 150) {
+        levelName = "13 级";
+        desc = "台风";
+    } else if (windSpeed >= 150 && windSpeed < 167) {
+        levelName = "14 级";
+        desc = "强台风";
+    } else if (windSpeed >= 167 && windSpeed < 184) {
+        levelName = "15 级";
+        desc = "强台风";
+    } else if (windSpeed >= 184 && windSpeed < 202) {
+        levelName = "16 级";
+        desc = "超强台风";
+    } else if (windSpeed >= 202 && windSpeed <= 220) {
+        levelName = "17 级";
+        desc = "超强台风";
+    } else {
+        levelName = "未知";
+        desc = "未知";
+    }
+
+    return { levelName, desc };
 }
