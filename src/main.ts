29a6d032f81ee80ff4476500e2935bea7c84f6bc
Sakunoakarinn <43726721+actredphos2017@users.noreply.github.com>
2024-03-28 12:36:27 +0800

    导包 pinia

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import App from './App.vue'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import {createPinia} from "pinia"
 
 const app = createApp(App);
+const pinia = createPinia();
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
 
 app.use(ElementPlus);
+app.use(pinia);
 app.mount('#app');
